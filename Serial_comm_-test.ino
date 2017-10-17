c4b87e1f6a17d8f3ac0f6fb2d201e2c1c0bbce3a
Jason Prabu. L <32505007+Jason-Prabu@users.noreply.github.com>
2017-10-17 20:00:32 +0530

    Add files via upload

---
+++ Serial_comm_-test.ino
@@ -0,0 +1,15 @@
+void setup() {
+
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+  Serial.println("Hello Raspberry");
+  delay(1000);
+  
+  // put your main code here, to run repeatedly:
+
+}
